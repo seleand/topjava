d23c49ce505ca96ac9143ce53ecaea1d9dcf3b03
seleand <seleand_rabota@mail.ru>
2016-09-21 21:39:25 +0300

    InMemoryUserRepositoryImpl

--- src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -6,39 +6,52 @@ import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * GKislin
  * 15.06.2015.
  */
 @Repository
-public class MockUserRepositoryImpl implements UserRepository {
-    private static final Logger LOG = LoggerFactory.getLogger(MockUserRepositoryImpl.class);
+public class InMemoryUserRepositoryImpl implements UserRepository {
+    private static final Logger LOG = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
+    private Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
 
     @Override
     public boolean delete(int id) {
         LOG.info("delete " + id);
-        return true;
+        User user = repository.remove(id);
+        return (user!=null);
     }
 
     @Override
     public User save(User user) {
         LOG.info("save " + user);
+        if (user.isNew()){
+            user.setId(counter.incrementAndGet());
+        }
+        repository.put(user.getId(),user);
         return user;
     }
 
     @Override
     public User get(int id) {
         LOG.info("get " + id);
-        return null;
+        return repository.get(id);
     }
 
     @Override
     public List<User> getAll() {
         LOG.info("getAll");
-        return Collections.emptyList();
+        List<User> users = new ArrayList<>(repository.values());
+        users.sort((o1, o2) -> o1.getName().compareTo(o2.getName()));
+        return users;
     }
 
     @Override
