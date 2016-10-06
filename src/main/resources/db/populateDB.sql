76ee4404a943c2066c8068dc1dae1255acd072b9
seleand <seleand_rabota@mail.ru>
2016-10-06 16:59:35 +0300

    hw2_add_db_meals

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -11,3 +11,8 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (description, calories, user_id) VALUES
+  ('user_meal_1', 100, 100000),
+  ('user_meal_2', 150, 100000),
+  ('admin_meal_1', 200, 100001);
